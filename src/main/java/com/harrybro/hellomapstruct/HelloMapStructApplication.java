71f931ed15c8f3e1baac7967fbd44efc487fc454
Harry-Bro <likeharrybro@gmail.com>
2021-09-10 10:33:01 +0900

    :tada: Initialize project

---
+++ src/main/java/com/harrybro/hellomapstruct/HelloMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.harrybro.hellomapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMapStructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMapStructApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harrybro/hellomapstruct/HelloMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrybro.hellomapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMapStructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
